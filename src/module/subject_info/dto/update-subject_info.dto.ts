a52989496280338b6013d876aed0bd26283aa694
simlenodir <nodirsmailov0@gmail.com>
2023-05-06 19:10:45 +0300

    deleted subject info module

--- src/module/subject_info/dto/update-subject_info.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSubjectInfoDto } from './create-subject_info.dto';
-
-export class UpdateSubjectInfoDto extends PartialType(CreateSubjectInfoDto) {}
--- src/module/subject_info/subject_info.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SubjectInfoService } from './subject_info.service';
-import { SubjectInfoController } from './subject_info.controller';
-
-@Module({
-  controllers: [SubjectInfoController],
-  providers: [SubjectInfoService]
-})
-export class SubjectInfoModule {}
